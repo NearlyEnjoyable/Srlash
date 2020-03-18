565d4085bc6834f03ad14f93eaf01813d9ad3c8c
NearlyEnjoyable <kalocsai.richard.r@gmail.com>
2020-03-18 20:25:40 +0100

    Migrated to Clap

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,15 @@
     Srlash - Splash screen written in Rust
     Colored output
     Reads file as frames which are separated.
-    
+
     TODO: Maybe add caching to tmp
-    Arguments: file; index of file (Maybe more?: looping time)
+    neofetch like statistics
+    listen to interrupts
+    Arguments: file; index of file, caching, Terminal sequences (Maybe more?: looping time)
 */
 
 use std::process::Command;
 use std::thread;
-use std::sync::mpsc::{Receiver, sync_channel};
 
 use std::io::Read;
 use std::fmt::Write;
@@ -30,7 +31,7 @@ const TS: [&str; 18] = /* Terminal Sequences */ [
     "\x1b[38;5;6m", // dark cyan
     "\x1b[38;5;7m", // light gray
     "\x1b[8m", // THIS sets hidden attribute
-    "\x1b[38;5;8m", // grey 
+    "\x1b[38;5;8m", // grey
     "\x1b[38;5;9m", // red
     "\x1b[38;5;10m",// green
     "\x1b[38;5;11m",// yellow
@@ -41,16 +42,12 @@ const TS: [&str; 18] = /* Terminal Sequences */ [
 
 ];
 
-
 static mut WIDE: usize = 0;
 static mut TALL: usize = 0;
-static mut RX_PONTER: *const Receiver<bool> = 0 as *const Receiver<bool>; // Why am I doing this??
 
 fn main() {
-
-    let (tx, rx) = sync_channel::<bool>(0);
-    thread::spawn(move|| { //get and assign terminal size
-        let output = String::from_utf8(Command::new("stty")
+    let (file, index, cache) = parse::cli();
+    let output = String::from_utf8(Command::new("stty")
             .arg("size")
             .arg("-F")
             .arg("/dev/stderr")
@@ -62,32 +59,6 @@ fn main() {
             TALL = formatted_output.next().unwrap();
             WIDE = formatted_output.next().unwrap();
         }
-        tx.send(true).unwrap();
-    });
-    
-    let mut index = 0;
-    let mut file = String::from(format!("{}{}", env::current_dir().unwrap()
-                                        .to_str().unwrap(), "/srlash"));
-    println!("{}", file);
-
-    let args: Vec<_> = env::args_os().map(|x| x.to_str().unwrap().to_string()).collect();
-    for a in args.iter() {
-        println!("{}", a);
-    }
-    if args.iter().any(|x| x == "-h") { // This is a hot mess, needs refactoring
-        print!("{}", HLP);
-        std::process::exit(0);
-    } if args.iter().any(|x| x == "-f") { // File path
-        file = args.iter().skip_while(|&x| !(x == "-f")).nth(1)
-            .expect("-f flag present but no path to file is given.").to_string();
-    } if args.iter().any(|x| x == "-i") { // Index
-        index = args.iter().skip_while(|&x| !(x == "-i")).nth(1).expect("-i flag is present but no index is given.")
-            .trim().parse().expect("Invalid index number.");
-    } // Could've used macros but too late
-    
-    unsafe {
-        RX_PONTER = &rx as *const Receiver<bool>;
-    }
 
     let path = std::path::Path::new(&file); //TODO: default file location where binary is run from
     let art = parse::get_art(path, index); //Get the desired art
@@ -98,7 +69,7 @@ fn main() {
     }
     println!("\x1b[?25h"); // Show cursor
     Command::new("clear").spawn().expect("Program paniced"); // Call "clear" -> no artifacts
-    
+
 }
 
 fn draw(art: &str) {
@@ -108,11 +79,7 @@ fn draw(art: &str) {
     let mut itr = TS.iter().cycle();
     let mut earlier_seq = TS[0];
     print!("\x1b[2J");
-    unsafe {
-        (*RX_PONTER).recv().unwrap(); // All of this to not pass WIDE and TALL. Worth it.
-        WIDE = ((WIDE as isize -max as isize) / 2) as usize;
-        TALL = ((TALL as isize - (neg_line_num * -1)) / 2) as usize;
-    }
+
     let mut str_buf: String = String::with_capacity(art.len() + (neg_line_num * -13) as usize);
     loop { // TODO: Should rework cursor jumping with ANSI save cursor position.
         let seq = itr.next().unwrap();
@@ -131,7 +98,7 @@ fn draw(art: &str) {
                     (frame_offset as isize + rand + line_offset as isize) as usize //all good
                 };
                 unsafe { // because of globals
-                    write!(&mut str_buf, "\x1b[0m{:>vpad$}{}{}\x1b[0m{}{}\n","", seq, 
+                    write!(&mut str_buf, "\x1b[0m{:>vpad$}{}{}\x1b[0m{}{}\n","", seq,
                              &text[0..offset], earlier_seq, &text[offset..], vpad=WIDE).unwrap();
                 }
             }
@@ -148,9 +115,7 @@ fn animate(art: Vec<String>) {
         let art = &art;
         for i in art {
             thread::sleep(std::time::Duration::from_millis(300));
-            print!("{}\x1b[2J", i); 
+            print!("{}\x1b[2J", i);
         }
     }
-} 
-
-const HLP: &str = "Srlash - Animated splash written in Rust.\n -h Displays this text and exits.\n -f To change the default file location.\n -i To change the default index number in the file. Defaults to the first.\n";
+}
--- src/parse.rs
+++ src/parse.rs
@@ -1,8 +1,54 @@
 //Parse module
 /* Parses a given file and returns the frames */
 
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::fs;
+
+use clap::{Arg, App};
+use clap::{crate_version, crate_authors, crate_description};
+
+pub fn cli() -> (PathBuf, usize, bool) {
+    let cli = App::new("Srlash")
+        .version(crate_version!()) /* Macro to get cargo crate info. No need to update here */
+        .author(crate_authors!())
+        .about(crate_description!())
+        .arg(Arg::with_name("Cache")
+            .short("c")
+            .long("cache")
+            .help("Enables caching thus improving performance. Recommended"))
+        .arg(Arg::with_name("File")
+            .short("f")
+            .long("file")
+            .required(true)
+            .index(1)
+            .takes_value(true)
+            .value_name("FILE")
+            .help("Specifies input file"))
+        .arg(Arg::with_name("Index")
+            .short("i")
+            .long("index")
+            .required(true)
+            .index(2)
+            .takes_value(true)
+            .value_name("INDEX")
+            .help("Index art number from set file. Must be number"))
+        .get_matches();
+
+    let mut file = PathBuf::new();
+    file.push(cli.value_of("File").unwrap());
+
+    if !file.is_absolute() {
+        /* Then make it absolute! */
+        let mut tmp_file = PathBuf::new();
+        tmp_file.push(String::from(format!("{}{}", std::env::current_dir().unwrap()
+                                        .to_str().unwrap(), file.to_str().unwrap())));
+        file = tmp_file;
+    }
+
+    let index = cli.value_of("Index").unwrap().parse().expect("Please use a number value for indexing.");
+
+    (file, index, cli.is_present("Cache"))
+}
 
 pub fn get_art(path: &Path, index: usize) -> Chunk {
     let buffer = fs::read_to_string(path).expect("File can't be opened or missing");
